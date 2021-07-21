c347f647a32762bffcc782f09f27432e9b9f663e
Yi Shen <yshensh@gmail.com>
2021-07-21 10:52:56 -0400

    restaurant managementa and reservation design

---
+++ src/main/java/restaurant/reservation/Meal.java
@@ -0,0 +1,13 @@
+package restaurant.reservation;
+
+public class Meal {
+    private final float price;
+
+    public Meal(float price) {
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+}
---
+++ src/main/java/restaurant/reservation/Party.java
@@ -0,0 +1,13 @@
+package restaurant.reservation;
+
+public class Party {
+    private final int size;
+
+    public Party(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+}
